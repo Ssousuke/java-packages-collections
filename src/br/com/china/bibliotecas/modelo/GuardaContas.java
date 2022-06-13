9c705f0ff5968cf8f96203c786930f66f282c8d3
Wesley Farias <wesleysousukee@gmail.com>
2022-06-13 11:47:35 -0300

    guarda contas pt1

---
+++ src/br/com/china/bibliotecas/modelo/GuardaContas.java
@@ -0,0 +1,9 @@
+package br.com.china.bibliotecas.modelo;
+
+public class GuardaContas {
+    private Conta[] fererencias;
+
+    public GuardaContas() {
+        this.fererencias = new Conta[10];
+    }
+}
---
+++ src/br/com/china/bibliotecas/teste/TesteGuardaContas.java
@@ -0,0 +1,7 @@
+package br.com.china.bibliotecas.teste;
+
+public class TesteGuardaContas {
+    public static void main(String[] args) {
+        
+    }
+}
