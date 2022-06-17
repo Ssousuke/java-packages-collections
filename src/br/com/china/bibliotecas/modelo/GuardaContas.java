599a5cdd6d17d05c9170033d6eec20a9a41eb2e7
Wesley Farias <wesleysousukee@gmail.com>
2022-06-16 23:53:10 -0300

    guarda contas pt2 final array

--- src/br/com/china/bibliotecas/modelo/GuardaContas.java
+++ src/br/com/china/bibliotecas/modelo/GuardaContas.java
@@ -2,8 +2,23 @@ package br.com.china.bibliotecas.modelo;
 
 public class GuardaContas {
     private Conta[] fererencias;
+    private int posicao;
 
     public GuardaContas() {
         this.fererencias = new Conta[10];
+        this.posicao = 0;
+    }
+
+    public void adiciona(Conta ref) {
+        this.fererencias[this.posicao] = ref;
+        this.posicao++;
+    }
+
+    public int getQuantidadeDeElementos() {
+        return this.posicao;
+    }
+
+    public Conta getReferenciaConta(int posicao) {
+        return this.fererencias[posicao];
     }
 }
--- src/br/com/china/bibliotecas/teste/TesteGuardaContas.java
+++ src/br/com/china/bibliotecas/teste/TesteGuardaContas.java
@@ -1,7 +1,23 @@
 package br.com.china.bibliotecas.teste;
 
+import br.com.china.bibliotecas.modelo.Conta;
+import br.com.china.bibliotecas.modelo.ContaCorrente;
+import br.com.china.bibliotecas.modelo.GuardaContas;
+
 public class TesteGuardaContas {
     public static void main(String[] args) {
-        
+        GuardaContas guardaContas = new GuardaContas();
+
+        Conta cc = new ContaCorrente(22, 10);
+        guardaContas.adiciona(cc);
+
+        Conta cc2 = new ContaCorrente(23, 12);
+        guardaContas.adiciona(cc2);
+
+        int tamanho = guardaContas.getQuantidadeDeElementos();
+        System.out.println(tamanho);
+
+        Conta ref = guardaContas.getReferenciaConta(1);
+        System.out.println(ref.getNumero());
     }
 }
